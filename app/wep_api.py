a6e3a54c6f91aff671dde566c06962ac0e260da8
Luis Vilella <luivilella@gmail.com>
2018-06-12 01:52:09 -0300

    feat(backend): wep api
    This is the first version of WEB API.
    The idea of this API is to be very simple, just receive data from http(s) requests send to the service and return the services values to users.

---
+++ app/wep_api.py
@@ -0,0 +1,77 @@
+from bottle import Bottle, request, response
+from environs import Env
+
+from services.pipedrive_organization import PipeDriveOrganizationService
+from utils.ip_info import get_ip_info
+
+env = Env()
+GEOLOCATION_BACKEND_SORT = env.bool('GEOLOCATION_BACKEND_SORT', False)
+
+service = PipeDriveOrganizationService()
+app = Bottle()
+
+
+@app.route('/organizations', method='GET')
+def search():
+    search = request.GET.get('search', '').strip()
+    getsort = request.GET.get('getsort', '').strip()
+    geolocation_sort = None
+    if getsort:
+        lat, lng = getsort.split(',')
+        geolocation_sort = (float(lat), float(lng))
+    elif GEOLOCATION_BACKEND_SORT:
+        ip_info = get_ip_info(request.environ.get('REMOTE_ADDR'))
+        lat, lng = ip_info.get('lat'), ip_info.get('lon')
+        if lat is not None and lng is not None:
+            geolocation_sort = (lat, lng)
+    try:
+        organizations = list(
+            service.search(search, geolocation_sort)
+        )
+    except ConnectionError as e:
+        response.status = 400
+        return dict(success=False, error=str(e))
+
+    return dict(success=True, data=organizations)
+
+
+@app.route('/organizations', method='POST')
+def create():
+    json_data = request.json
+    post_data, field_type = json_data['data'], json_data.get('field_type', {})
+
+    data = {}
+    for field, value in post_data.items():
+        data[field] = value
+        if field_type.get(field) == 'address':
+            data[field] = f'{value["lat"]},{value["lng"]}'
+
+    try:
+        organization = service.create(data, field_type)
+    except ConnectionError as e:
+        response.status = 400
+        return dict(success=False, error=str(e))
+
+    return dict(success=True, data=organization)
+
+
+@app.route('/organizations/<organization_id:int>', method='GET')
+def detail(organization_id):
+    try:
+        organization = service.detail(organization_id)
+    except ConnectionError as e:
+        response.status = 400
+        return dict(success=False, error=str(e))
+
+    return dict(success=True, data=organization)
+
+
+@app.route('/organizations-fields', method='GET')
+def fields():
+    try:
+        fields = service.fields()
+    except ConnectionError as e:
+        response.status = 400
+        return dict(success=False, error=str(e))
+
+    return dict(success=True, data=list(fields))
---
+++ app/wsgi.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+from bottle import run
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+from wep_api import app
+
+application = app
+
+
+if __name__ == '__main__':
+    run(application, host='0.0.0.0', port=8080, reloader=True, debug=True)
