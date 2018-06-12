19d6d2bef81af2362c413ab21b9c515ccc379c98
Luis Vilella <luivilella@gmail.com>
2018-06-12 20:34:57 -0300

    fix(backend): services
    When filtering by geolocation returns as lasts elements organizations that does not have lat long

--- app/services/pipedrive_organization.py
+++ app/services/pipedrive_organization.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from pipedrive import PipeDrive
 from utils.geo_sort import geo_sorted
 
@@ -15,18 +16,30 @@ class PipeDriveOrganizationService:
         if not geolocation_sort:
             return organizations
 
-        organizations_filtered = filter(
-            lambda obj: obj.get('address'), organizations
-        )
+        def valid_lat_long(organization):
+            address = organization.get('address') or ''
+            params = address.split(',')
+            if len(params) == 2 and all(params):
+                return True
+            return False
+
+        def invalid_lat_lng(organization):
+            return not valid_lat_long(organization)
 
         def get_lat_lng(obj):
             latitude, longitude = obj['address'].split(',')
             return (float(latitude), float(longitude))
-        organizations_sorted = geo_sorted(
-            geolocation_sort, organizations_filtered, get_lat_lng
+
+        organizations_sorted_by_lat_lng = geo_sorted(
+            geolocation_sort,
+            filter(valid_lat_long, organizations),
+            get_lat_lng
         )
 
-        return organizations_sorted
+        return chain(
+            organizations_sorted_by_lat_lng,
+            filter(invalid_lat_lng, organizations)
+        )
 
     def detail(self, organization_id):
         return self.pd.organization.detail(organization_id)
